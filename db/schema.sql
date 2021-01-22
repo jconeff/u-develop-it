513b0c90a9431c47a8f23925848cfbfa9ba846ae
jconeff <jessicaconeff@gmail.com>
2021-01-22 16:06:06 -0500

    added new files to organize

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,12 @@
+CREATE TABLE voters (
+  id INTEGER PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+
 DROP TABLE IF EXISTS parties;
 DROP TABLE IF EXISTS candidates;
 
